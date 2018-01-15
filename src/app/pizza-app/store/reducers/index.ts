62ce66335344e667d8791c219aea8b323cb492e2
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-14 23:04:26 -0500

    create our first selectore to render our first pizza in aour angular application

--- src/app/pizza-app/store/reducers/index.ts
+++ src/app/pizza-app/store/reducers/index.ts
@@ -1,4 +1,6 @@
-import { ActionReducerMap } from '@ngrx/store'; // check types for us based on provided argument state type
+import { ActionReducerMap ,
+  createFeatureSelector,
+  createSelector} from '@ngrx/store'; // ActionReducerMap check types for us based on provided argument state type
 
 import * as fromPizzas from './pizzas.reducer';
 
@@ -11,3 +13,25 @@ export interface PizzasState {
 export const reducers: ActionReducerMap<PizzasState> = {
     pizzas: fromPizzas.reducer,
   };
+// create base property for pizzas selector
+  export const getPizzasState = createFeatureSelector<PizzasState>(
+    // 'pizzas' comes from pizza-app-module.ts StoreModule.forFeature('pizzas', reducers),
+    'pizzas'
+  );
+
+  // pizzas state
+export const getPizzaState = createSelector(
+  getPizzasState,
+  (state: PizzasState) => state.pizzas
+);
+
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
