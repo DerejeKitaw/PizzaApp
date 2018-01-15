3d94505a333b5e320dfaace15ecb573aea8b2db2
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-14 19:06:58 -0500

    define three actions will hapen when pizza app load

--- src/app/pizza-app/store/actions/pizzas.action.ts
+++ src/app/pizza-app/store/actions/pizzas.action.ts
@@ -1,1 +1,8 @@
 import {Action } from '@ngrx/store';
+
+// To load pizzas dispatch LOAD_PIZZAS actions
+export const LOAD_PIZZAS = '[Pizzas] Load Pizzas'; // '[Pizzas] Load Pizzas' can be any string
+// If load pizzas fails dispatch LOAD_PIZZAS_FAIL  actions
+export const LOAD_PIZZAS_FAIL = '[Pizzas] Load Pizzas Fail';
+// If load pizzas success dispatch LOAD_PIZZAS_SUCCESS  actions
+export const LOAD_PIZZAS_SUCCESS = '[Pizzas] Load Pizzas Success';
