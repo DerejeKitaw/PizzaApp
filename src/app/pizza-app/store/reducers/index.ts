3ec67d5425fb96b392444d938b855bfab7ed6ef7
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-14 19:45:06 -0500

    register our reducer

--- src/app/pizza-app/store/reducers/index.ts
+++ src/app/pizza-app/store/reducers/index.ts
@@ -1,7 +1,13 @@
-
+import { ActionReducerMap } from '@ngrx/store'; // check types for us based on provided argument state type
 
 import * as fromPizzas from './pizzas.reducer';
 
 export interface PizzasState {
   pizzas: fromPizzas.PizzaState;
 }
+
+// This will define how our reducers look like
+// ActionReducerMap will check the state
+export const reducers: ActionReducerMap<PizzasState> = {
+    pizzas: fromPizzas.reducer,
+  };
