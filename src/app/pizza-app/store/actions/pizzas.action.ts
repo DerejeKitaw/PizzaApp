962347389337c37c391fca147f08a18fe847c43b
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-14 19:13:00 -0500

    Define action creaters

--- src/app/pizza-app/store/actions/pizzas.action.ts
+++ src/app/pizza-app/store/actions/pizzas.action.ts
@@ -1,3 +1,4 @@
+import { Pizza } from '../../../models/pizza.model';
 import {Action } from '@ngrx/store';
 
 // To load pizzas dispatch LOAD_PIZZAS actions
@@ -6,3 +7,17 @@ export const LOAD_PIZZAS = '[Pizzas] Load Pizzas'; // '[Pizzas] Load Pizzas' can
 export const LOAD_PIZZAS_FAIL = '[Pizzas] Load Pizzas Fail';
 // If load pizzas success dispatch LOAD_PIZZAS_SUCCESS  actions
 export const LOAD_PIZZAS_SUCCESS = '[Pizzas] Load Pizzas Success';
+
+export class LoadPizzas implements Action {
+    readonly type = LOAD_PIZZAS;
+  }
+
+  export class LoadPizzasFail implements Action {
+    readonly type = LOAD_PIZZAS_FAIL;
+    constructor(public payload: any) {} // Pass payload message to the server if there is an error
+  }
+
+  export class LoadPizzasSuccess implements Action {
+    readonly type = LOAD_PIZZAS_SUCCESS;
+    constructor(public payload: Pizza[]) {} // import Pizza model
+  }
