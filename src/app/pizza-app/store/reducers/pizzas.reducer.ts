f50ca9a3f9255adfb02f6006fd59fa61ce0bb5eb
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-14 19:23:43 -0500

    define piza state interface for type cheking

--- src/app/pizza-app/store/reducers/pizzas.reducer.ts
+++ src/app/pizza-app/store/reducers/pizzas.reducer.ts
@@ -1,7 +1,13 @@
+import { Pizza } from '../../../models/pizza.model';
 
+export interface PizzaState {
+  data: Pizza[];
+  loaded: boolean;
+  loading: boolean;
+}
 
-export const initialState = {
-    data: [],
-    loaded: false,
-    loading: false,
-  };
+export const initialState: PizzaState = {
+  data: [],
+  loaded: false,
+  loading: false,
+};
