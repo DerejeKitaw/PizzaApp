986a4abc5c474c2f2ee7a50570d95b8dd57d5302
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-17 23:16:55 -0500

    update angular UI - connect our new selector

--- src/app/pizza-app/pizza-detail/pizza-detail.component.ts
+++ src/app/pizza-app/pizza-detail/pizza-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import * as fromStore from '../store';
+
 import { Topping } from '../../models/topping.model';
 import { Pizza } from '../../models/pizza.model';
 
@@ -11,13 +15,15 @@ import { Pizza } from '../../models/pizza.model';
   styleUrls: ['./pizza-detail.component.scss']
 })
 export class PizzaDetailComponent implements OnInit {
-  pizza: Pizza;
+  pizza$: Observable<Pizza>;
   visualise: Pizza;
   toppings: Topping[];
 
-  constructor() {}
+  constructor(private store: Store<fromStore.PizzasState>) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
+  }
 
   onSelect(event: number[]) {}
 
