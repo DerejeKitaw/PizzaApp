6a76aa5e999f58298a76e45889d03b444dc6ff28
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-17 23:47:22 -0500

    setup topping reducer

--- src/app/pizza-app/store/reducers/index.ts
+++ src/app/pizza-app/store/reducers/index.ts
@@ -1,16 +1,19 @@
-import {
-  ActionReducerMap, createFeatureSelector} from '@ngrx/store'; // ActionReducerMap check types for us based on provided argument state type
+// ActionReducerMap check types for us based on provided argument state type
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store'; 
 
 import * as fromPizzas from './pizzas.reducer';
+import * as fromToppings from './toppings.reducer';
 
 export interface PizzasState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 // This will define how our reducers look like
 // ActionReducerMap will check the state
 export const reducers: ActionReducerMap<PizzasState> = {
-  pizzas: fromPizzas.reducer
+  pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer,
 };
 // create base property for pizzas selector
 export const getPizzasState = createFeatureSelector<PizzasState>(
