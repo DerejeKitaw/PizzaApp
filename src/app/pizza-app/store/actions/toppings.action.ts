37e6695c757470fa7e6c3bbf2cc3dc9dae04f20c
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-17 23:29:19 -0500

    create topping actions

---
+++ src/app/pizza-app/store/actions/toppings.action.ts
@@ -0,0 +1,28 @@
+import { Action } from '@ngrx/store';
+import { Topping } from '../../../models/topping.model';
+
+
+
+export const LOAD_TOPPINGS = '[Pizzas] Load Toppings';
+export const LOAD_TOPPINGS_FAIL = '[Pizzas] Load Toppings Fail';
+export const LOAD_TOPPINGS_SUCCESS = '[Pizzas] Load Toppings Success';
+
+export class LoadToppings implements Action {
+  readonly type = LOAD_TOPPINGS;
+}
+
+export class LoadToppingsFail implements Action {
+  readonly type = LOAD_TOPPINGS_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class LoadToppingsSuccess implements Action {
+  readonly type = LOAD_TOPPINGS_SUCCESS;
+  constructor(public payload: Topping[]) {}
+}
+
+// action types
+export type ToppingsAction =
+  | LoadToppings
+  | LoadToppingsFail
+  | LoadToppingsSuccess;
