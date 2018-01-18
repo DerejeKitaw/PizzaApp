33b7e15d0dd42f60999967a6ac69ccc401692a46
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-18 01:39:15 -0500

    dispatch initial visualize topping

--- src/app/pizza-app/pizza-detail/pizza-detail.component.ts
+++ src/app/pizza-app/pizza-detail/pizza-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
+
+
 import * as fromStore from '../store';
 
 import { Topping } from '../../models/topping.model';
@@ -22,7 +25,13 @@ export class PizzaDetailComponent implements OnInit {
   constructor(private store: Store<fromStore.PizzasState>) {}
 
   ngOnInit() {
-    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+    tap((pizza: Pizza = null) => {
+      const pizzaExists = !!(pizza && pizza.toppings);
+      const toppings = pizzaExists ? pizza.toppings.map(topping => topping.id) : [];
+      this.store.dispatch(new fromStore.VisualiseToppings(toppings));
+    })
+    );
     this.toppings$ = this.store.select(fromStore.getAllToppings);
     this.visualise$ = this.store.select(fromStore.getPizzaVisualised);
   }
